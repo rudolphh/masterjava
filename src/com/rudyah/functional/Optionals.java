1bef6264b2115854a46a9173c726074f78edd961
Rudy A. Hernandez <rudolpharthur@gmail.com>
2022-06-15 18:35:23 -0700

    optionals

--- src/com/rudyah/functional/Optionals.java
+++ src/com/rudyah/functional/Optionals.java
@@ -1,5 +1,6 @@
 package com.rudyah.functional;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public class Optionals {
@@ -14,8 +15,19 @@ public class Optionals {
     public static void main(String[] args) {
         Optional<Double> average = Optional.empty();
         //average.get();// NoSuchElementException
+        System.out.println(average.orElse(0.0));
+        Double orElseGet = average.orElseGet(() -> 0.0);// excessive in this case but for objects useful
+
+        try {
+            //System.out.println(average.orElseThrow());
+            System.out.println(average.orElseThrow(()-> new NoSuchElementException("No average?!? What happened")));
+        } catch (NoSuchElementException e) {
+            System.out.println(e.getMessage());
+        }
 
         System.out.println(average(90, 100));
+        average(50, 80).ifPresent(System.out::println);// ifPresent(Consumer)
+
         System.out.println(average());
     }
 }
